31db340291b96a6c758a1d9ae8c2890260ea90e0
tomibzan <thomflowen@gmail.com>
2026-09-09 14:51:49 +0300

    fix: add HttpResponse import and sitemaps to INSTALLED_APPS

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 import logging
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse, HttpResponse
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.conf import settings
